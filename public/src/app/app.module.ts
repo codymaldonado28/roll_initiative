fb980417bd7a7f5031953e22abec7ba1a0c216b1
Drew Bryar <abryar89@gmail.com>
2019-12-16 17:20:47 -0600

    Updated app.module.ts and added its new Components

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpService } from './http.service';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +18,11 @@ import { CharacterSheetComponent } from './character-sheet/character-sheet.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
