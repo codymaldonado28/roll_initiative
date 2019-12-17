86be0b3598f30ccdcb9a4a5c5eb9e2ba72ef2b6c
jeremydabbs <56282992+jeremydabbs@users.noreply.github.com>
2019-12-17 16:05:03 -0600

    Update to Stat Component

--- public/src/app/stat/stat.component.ts
+++ public/src/app/stat/stat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-stat',
@@ -9,10 +10,41 @@ import { HttpService } from '../http.service';
 export class StatComponent implements OnInit {
 
   constructor(
-    private _httpService: HttpService
+    private _httpService: HttpService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) { }
 
+  editCharacter: any;
+  errors = [];
+
   ngOnInit() {
+    this._route.params.subscribe((params: Params) => {
+      this.getEditCharacter(params['id']);
+    })
   }
 
+  getEditCharacter(id){
+    let obs = this._httpService.getOneCharacter(id);
+    obs.subscribe(data => {
+      if(data['results']){
+        this.editCharacter = data['results'];
+      }
+    })
+  }
+
+  updateAuthor(){
+    this.errors = [];
+    let obs = this._httpService.updateCharacter(this.editCharacter);
+    obs.subscribe(data => {
+      if(data['results']){
+        this._router.navigate(['/']);
+      }
+      else if(data['errors']){
+        for(var key in data['errors']){
+          this.errors.push(data['errors'][key]['message']);
+        }
+      }
+    })
+  }
 }
